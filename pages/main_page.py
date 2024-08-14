efd205d76dd3da025ebc24e8ffc47eb0e66dee82
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-14 14:49:43 +0300

    add assert in method should_be_login_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,3 +1,7 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
 class MainPage(BasePage):
 
     def go_to_login_page(self):
@@ -5,4 +9,5 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), \
+            "Login link is not presented"
