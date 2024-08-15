7217f9b608bd1e2cd5bcb433d565dd3b66520346
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-15 18:14:44 +0300

    add class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,12 @@ class MainPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
     # LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+
+class ProductPageLocators():
+
+    BASKET_ADD = (By.CSS_SELECTOR, ".btn-primary.btn-add-to-basket")
+    NAME_BOOK = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRICE_BOOK = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    NAME_BOOK_IN_BASKET = (By.CSS_SELECTOR, ".alertinner strong")
+    PRICE_BASKET = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
