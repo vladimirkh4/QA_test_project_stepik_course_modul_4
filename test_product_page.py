7405d3e01f12e057ca1d303ad761495ba64b130e
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-15 18:59:34 +0300

    add parametrize decorator

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,15 @@
+import pytest
 from .pages.product_page import ProductPage
 
+head = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+links = [f"{head}{num}" for num in range(10)]
+links[7] = pytest.param(links[7], marks=pytest.mark.xfail)
 
+
+@pytest.mark.parametrize('link', links)
 def test_guest_can_add_product_to_basket(browser):
     # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
 
     page = ProductPage(browser, link)
     page.open()
