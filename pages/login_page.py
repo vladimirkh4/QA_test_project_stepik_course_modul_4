a0a516fdf095bb7a7a714366183513f7d827f208
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-15 22:34:42 +0300

    refactoring method should_be_login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,9 +4,11 @@ from .locators import MainPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+        check_list = [self.should_be_login_url,
+                      self.should_be_login_form,
+                      self.should_be_register_form]
+
+        self.run_test_with_multy_methods(check_list)
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
