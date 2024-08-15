e74cd72f36c6d8a7fae8035f408bf077f2d37b07
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-15 22:50:01 +0300

    refactoring test_guest_can_add_product_to_basket add three tests test_guest_cant_see_success_message_after_adding_product_to_basket, test_guest_cant_see_success_message, test_message_disappeared_after_adding_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,7 @@ links = [f"{head}{num}" for num in range(10)]
 links[7] = pytest.param(links[7], marks=pytest.mark.xfail)
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', links)
 def test_guest_can_add_product_to_basket(browser):
     # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -13,4 +14,31 @@ def test_guest_can_add_product_to_basket(browser):
 
     page = ProductPage(browser, link)
     page.open()
-    page.add_product_to_basket()
+    page.add_product_to_basket_with_check_basket()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.only_add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+
+    page = ProductPage(browser, link, 1)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.only_add_product_to_basket()
+    page.should_not_be_disappeared_success_message()
