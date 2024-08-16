32d765fbddc39e5ead2913aefca357183fa32f0c
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-16 11:31:08 +0300

    add class BasePageLocators and change class MainPageLocators for class LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,15 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    # LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
-    # LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
 
 
 class ProductPageLocators():
