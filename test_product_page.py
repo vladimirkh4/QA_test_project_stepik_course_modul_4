57a4e4aba10eded9066fa07aa68813b2abec7bb5
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-16 11:49:13 +0300

    add two tests: test_guest_should_see_login_link_on_product_page and test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 head = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
 links = [f"{head}{num}" for num in range(10)]
@@ -8,13 +9,14 @@ links[7] = pytest.param(links[7], marks=pytest.mark.xfail)
 
 @pytest.mark.skip
 @pytest.mark.parametrize('link', links)
-def test_guest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket(browser, link):
     # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
 
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket_with_check_basket()
+
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -42,3 +44,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.only_add_product_to_basket()
     page.should_not_be_disappeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
