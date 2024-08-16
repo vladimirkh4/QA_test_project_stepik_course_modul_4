d9ad352e34b91a3e549c4e908e3f25772360bffc
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-16 11:43:35 +0300

    add two methods from main_page.py and ordered methods in alphabet

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 
 
@@ -12,8 +13,31 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def call_function_with_try_exception(self, func):
+        try:
+            func()
+        except NoSuchElementException:
+            print(f'No search element for {func} function')
+            return 1
+        except AssertionError as err:
+            print(err)
+            return 1
+
+        return 0
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
 
     def is_element_present(self, how, what):
         try:
@@ -21,6 +45,30 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def run_test_with_multy_methods(self, methods_list):
+        check_counter = 0
+        for method in methods_list:
+            check_method = self.call_function_with_try_exception(method)
+            check_counter += check_method
+
+        if check_counter > 0:
+            raise ValueError(f"QUANTITY FAILED TESTS: {check_counter} FROM {len(methods_list)}")
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK),\
+            "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -35,41 +83,3 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
-
-    def call_function_with_try_exception(self, func):
-        try:
-            func()
-        except NoSuchElementException:
-            print(f'No search element for {func} function')
-            return 1
-        except AssertionError as err:
-            print(err)
-            return 1
-
-        return 0
-
-    def run_test_with_multy_methods(self, methods_list):
-        check_counter = 0
-        for method in methods_list:
-            check_method = self.call_function_with_try_exception(method)
-            check_counter += check_method
-
-        if check_counter > 0:
-            raise ValueError(f"QUANTITY FAILED TESTS: {check_counter} FROM {len(methods_list)}")
-
-    def is_not_element_present(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return True
-
-        return False
-
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-
-        return True
