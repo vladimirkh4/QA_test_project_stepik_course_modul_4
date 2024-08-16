bd2cf97f0083ae7dbc97a643769c718fd2d3ce2f
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-16 11:34:03 +0300

    change class MainPageLocators for class LoginPageLocators

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -16,10 +16,10 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM),\
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),\
             "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*MainPageLocators.REGISTER_FORM),\
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM),\
             "Register form is not presented"
