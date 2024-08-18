e7c0931e31dfe1e14e43a1d021c7a5f6f264aba3
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-18 15:40:19 +0300

    add class TestUserAddToBasketFromProductPage with two tests and setup

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 import pytest
+from time import time
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
@@ -79,3 +80,29 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.check_basket_with_multy_methods()
+
+
+@pytest.mark.user_add_to_basket
+class TestUserAddToBasketFromProductPage:
+    LINK = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time()) + "@fakemail.org"
+        password = 'Stepik_4_3'
+        page = LoginPage(browser, self.LINK)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, self.LINK, 1)
+        page.open()
+        page.should_not_be_success_message()
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, self.LINK)
+        page.open()
+        page.check_form_with_multy_message_about_add_to_basket()
+        page.should_be_authorized_user()
