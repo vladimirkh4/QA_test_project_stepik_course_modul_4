1db7dfc63358829d9b219bfae464bd0a9311f6a8
Vladimir Bashlykov <vlad_29_72@rambler.ru>
2024-08-18 15:33:59 +0300

    add new method register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,14 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        self.go_to_login_page()
+
+        self.browser.find_element(*LoginPageLocators.EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_REPEAT).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
     def should_be_login_page(self):
         check_list = [self.should_be_login_url,
                       self.should_be_login_form,
